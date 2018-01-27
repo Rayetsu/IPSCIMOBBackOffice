eab3e3dc68b5ba202bb6bd570c82f638fd9a5f40
Rayetsu <gabriel25asilva@gmail.com>
2018-01-27 17:35:26 +0000

    Gestao dos alunos estrangeiros

---
+++ IPSCIMOBBackOffice/ForeignStudents.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPSCIMOBBackOffice
+{
+    [Serializable]
+    class ForeignStudents : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            PropertyChanged?.Invoke(this, e);
+        }
+
+        int id;
+        public int Id
+        {
+            get { return Id; }
+            set
+            {
+                Id = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("Id"));
+
+            }
+        }
+
+        string nome;
+        public string Nome
+        {
+            get { return nome; }
+            set
+            {
+                nome = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("Nome"));
+            }
+        }
+
+        string nacionalidade;
+        public string Nacionalidade
+        {
+            get { return nacionalidade; }
+            set
+            {
+                nacionalidade = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("Nacionalidade"));
+            }
+        }
+
+        string email;
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                email = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("Email"));
+            }
+        }
+
+        DateTime dataDeNascimento;
+        public DateTime DataDeNascimento
+        {
+            get { return dataDeNascimento; }
+            set
+            {
+                dataDeNascimento = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("DataDeNascimento"));
+            }
+        }
+
+        string escolaIPSECurso;
+        public string EscolaIPSECurso
+        {
+            get { return escolaIPSECurso; }
+            set
+            {
+                escolaIPSECurso = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("EscolaIPSECurso"));
+            }
+        }
+
+        string morada;
+        public string Morada
+        {
+            get { return morada; }
+            set
+            {
+                morada = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("Morada"));
+            }
+        }
+
+        int numeroDaPorta;
+        public int NumeroDaPorta
+        {
+            get { return numeroDaPorta; }
+            set
+            {
+                numeroDaPorta = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("NumeroDaPorta"));
+
+            }
+        }
+
+        string andar;
+        public string Andar
+        {
+            get { return andar; }
+            set
+            {
+                andar = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("Andar"));
+            }
+        }
+
+        string cidade;
+        public string Cidade
+        {
+            get { return cidade; }
+            set
+            {
+                cidade = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("Cidade"));
+            }
+        }
+
+        string distrito;
+        public string Distrito
+        {
+            get { return distrito; }
+            set
+            {
+                distrito = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("Distrito"));
+            }
+        }
+
+        string codigoPostal;
+        public string CodigoPostal
+        {
+            get { return codigoPostal; }
+            set
+            {
+                codigoPostal = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("CodigoPostal"));
+            }
+        }
+
+        string universidade;
+        public string Universidade
+        {
+            get { return universidade; }
+            set
+            {
+                universidade = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("Distrito"));
+            }
+        }
+
+        int telefone;
+        public int Telefone
+        {
+            get { return telefone; }
+            set
+            {
+                telefone = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("Telefone"));
+
+            }
+        }
+
+        bool isBolseiro;
+        public bool IsBolseiro
+        {
+            get { return isBolseiro; }
+            set
+            {
+                isBolseiro = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("IsBolseiro"));
+
+            }
+        }
+
+        bool partilhaMobilidade;
+        public bool PartilhaMobilidade
+        {
+            get { return partilhaMobilidade; }
+            set
+            {
+                partilhaMobilidade = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("PartilhaMobilidade"));
+
+            }
+        }
+
+        bool isFuncionario;
+        public bool IsFuncionario
+        {
+            get { return isFuncionario; }
+            set
+            {
+                isFuncionario = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("IsFuncionario"));
+
+            }
+        }
+
+        bool isDadosVerificados;
+        public bool IsDadosVerificados
+        {
+            get { return isDadosVerificados; }
+            set
+            {
+                isDadosVerificados = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("Telefone"));
+
+            }
+        }
+
+        public ForeignStudents()
+        {
+        }
+
+        public ForeignStudents(ForeignStudents foreignStudent)
+        {
+            this.Id = foreignStudent.Id;
+            this.Nome = foreignStudent.Nome;
+            this.Nacionalidade = foreignStudent.Nacionalidade;
+            this.Email = foreignStudent.Email;
+            this.DataDeNascimento = foreignStudent.DataDeNascimento;
+            this.EscolaIPSECurso = foreignStudent.EscolaIPSECurso;
+            this.Morada = foreignStudent.Morada;
+            this.NumeroDaPorta = foreignStudent.NumeroDaPorta;
+            this.Andar = foreignStudent.Andar;
+            this.Cidade = foreignStudent.Cidade;
+            this.Distrito = foreignStudent.Distrito;
+            this.CodigoPostal = foreignStudent.CodigoPostal;
+            this.Universidade = foreignStudent.Universidade;
+            this.Telefone = foreignStudent.Telefone;
+            this.IsBolseiro = foreignStudent.IsBolseiro;
+            this.PartilhaMobilidade = foreignStudent.PartilhaMobilidade;
+            this.IsFuncionario = foreignStudent.IsFuncionario;
+            this.IsDadosVerificados = foreignStudent.IsDadosVerificados;
+        }
+
+    }
+}
--- IPSCIMOBBackOffice/GestorAlunosEstrangeiros.xaml.cs
+++ IPSCIMOBBackOffice/GestorAlunosEstrangeiros.xaml.cs
@@ -10,26 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace IPSCIMOBBackOffice
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Lógica interna para GestorAlunosEstrangeiros.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class GestorAlunosEstrangeiros : Window
     {
-        public MainWindow()
+        public GestorAlunosEstrangeiros()
         {
             InitializeComponent();
-        }
-
-        private void GestorAlunosEstrangeiros_OnClick(object sender, RoutedEventArgs e)
-        {
-            GestorAlunosEstrangeiros gAE = new GestorAlunosEstrangeiros();
-            gAE.Title = "Gestor de Alunos Estrangeiros";
-            gAE.ShowDialog();
         }
     }
 }
